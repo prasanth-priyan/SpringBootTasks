3ac857c12a37fafcf66efb4790ccc9cb5b587cdc
prasanth-priyan <prasanth.priyan@gmail.com>
2019-10-04 17:49:29 +0530

    Task-8

--- src/main/java/com/stackroute/MovieServiceApplication.java
+++ src/main/java/com/stackroute/MovieServiceApplication.java
@@ -1,9 +1,12 @@
 package com.stackroute;
 
+import com.stackroute.startup.StartupDatabaseSeederApproach2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StartupDatabaseSeederApproach2.class)
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/stackroute/startup/StartupDatabaseSeederApproach2.java
+++ src/main/java/com/stackroute/startup/StartupDatabaseSeederApproach2.java
@@ -4,13 +4,21 @@ import com.stackroute.domain.Movie;
 import com.stackroute.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:values.properties")
+@ConfigurationProperties(prefix = "movie")
 public class StartupDatabaseSeederApproach2 implements CommandLineRunner {
 
     private MovieRepository movieRepository;
     private Movie movie;
+
+    private int id2;
+    private String name2;
+    private String watch;
 
     @Autowired
     public void setMovieRepository(MovieRepository movieRepository) {
@@ -20,9 +28,21 @@ public class StartupDatabaseSeederApproach2 implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Movie movie = new Movie();
-        movie.setMovieId(2);
-        movie.setMovieName("Frozen");
-        movie.setMovieComments("Watched");
+        movie.setMovieId(id2);
+        movie.setMovieName(name2);
+        movie.setMovieComments(watch);
         movieRepository.save(movie);
+    }
+
+    public void setId2(int id2) {
+        this.id2 = id2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public void setWatch(String watch) {
+        this.watch = watch;
     }
 }
